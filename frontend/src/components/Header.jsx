451ebc519aaf6f13ad77e486e60602878bb96a10
BASSprogrammer <bariassalvo@gmail.com>
2026-02-13 13:17:44 -0300

    Update css style and guitar price

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,27 +3,27 @@ import "../assets/css/modedarklight.css";
 import Switch from "./Switch";
 import Menu from "./Menu";
 
-export default function Header({dataCart, deleteCart, emptyCart}){
+export default function Header({ dataCart, deleteCart, emptyCart }) {
     const nombretienda = "GuitarStore"
-    return(
+    return (
         <header className="sticky-menu">
-            <div className="header-container flex justify-between flex-wrap align-center">
+            <div className="header-container grid align-center">
                 <div className="header-logo left align-center">
                     <a href="/home" className="flex align-center" title="Ir al inicio">
-                    <img src={require("../assets/images/header/guitar.png")} alt="Logo" width={'50px'} height={'50px'}></img>
-                    <h1>{nombretienda}</h1> 
+                        <img src={require("../assets/images/header/guitar.png")} alt="Logo" width={'50px'} height={'50px'}></img>
+                        <h1>{nombretienda}</h1>
                     </a>
                 </div>
                 <div>
-                    <Menu/>
+                    <Menu />
                 </div>
             </div>
             <div className="darkmode-shop right flex align-center gap-2 position-absolute top-05 right-2">
-                <Switch/>
+                <Switch />
                 <ShoppingCart
-                    dataCart = {dataCart}
-                    deleteCart={deleteCart} 
-                    emptyCart = {emptyCart}
+                    dataCart={dataCart}
+                    deleteCart={deleteCart}
+                    emptyCart={emptyCart}
                 />
             </div>
         </header>
--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -3,23 +3,23 @@ import { faHome, faGuitar, faUser, faFile, faBars } from "@fortawesome/free-soli
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function Menu(){
+export default function Menu() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    return(
+    return (
         <div>
             <div>
                 {/* Overlay to close menu when clicking outside */}
                 {isMenuOpen && <div className="menu-overlay" onClick={() => setIsMenuOpen(false)}></div>}
                 <button className="menu-toggle-button" onClick={() => setIsMenuOpen(!isMenuOpen)} aria-label="Toggle menu">
-                    <FontAwesomeIcon icon={faBars} size="xs"/>
+                    <FontAwesomeIcon icon={faBars} size="xs" />
                 </button>
             </div>
-            <nav className={`header-menu${isMenuOpen ? ' menu-opened' : ' menu-closed'}`}>
+            <nav className={`header-menu flex justify-center align-center${isMenuOpen ? ' menu-opened' : ' menu-closed'}`}>
                 {isMenuOpen && <div className="menu-title center">Menu</div>}
-                <Link to="/home" className="menu-link" onClick={() => setIsMenuOpen(false)}>{isMenuOpen && <FontAwesomeIcon icon={faHome} size="sm" className="menu-icon"/>} Inicio</Link>
-                <Link to="/products" className="menu-link" onClick={() => setIsMenuOpen(false)}>{isMenuOpen && <FontAwesomeIcon icon={faGuitar} size="sm" className="menu-icon"/>}Productos</Link>
-                <Link to="/about" className="menu-link" onClick={() => setIsMenuOpen(false)}>{isMenuOpen && <FontAwesomeIcon icon={faUser} size="sm" className="menu-icon"/>}Nosotros</Link>
-                <Link to="/contact" className="menu-link" onClick={() => setIsMenuOpen(false)}>{isMenuOpen && <FontAwesomeIcon icon={faFile} size="sm" className="menu-icon"/>}Contacto</Link>
+                <Link to="/home" className="menu-link" onClick={() => setIsMenuOpen(false)}>{isMenuOpen && <FontAwesomeIcon icon={faHome} size="sm" className="menu-icon" />} Inicio</Link>
+                <Link to="/products" className="menu-link" onClick={() => setIsMenuOpen(false)}>{isMenuOpen && <FontAwesomeIcon icon={faGuitar} size="sm" className="menu-icon" />}Productos</Link>
+                <Link to="/about" className="menu-link" onClick={() => setIsMenuOpen(false)}>{isMenuOpen && <FontAwesomeIcon icon={faUser} size="sm" className="menu-icon" />}Nosotros</Link>
+                <Link to="/contact" className="menu-link" onClick={() => setIsMenuOpen(false)}>{isMenuOpen && <FontAwesomeIcon icon={faFile} size="sm" className="menu-icon" />}Contacto</Link>
             </nav>
         </div>
     )
